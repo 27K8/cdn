723578136a13eb87f8a5605d1be6089f646a970e
sysh8888 <sysh8888@126.com>
2023-04-18 15:50:57 +0800

    Create ver.js

---
+++ ver.js
@@ -0,0 +1,14 @@
+var verr="20230418";var down="http://mao.7080.wang";
+if(ver<verr){ 
+	window.alert("有新版本更新拉！版本号:"+verr+"请复制下载地址到游览器去下载吧!下载地址："+down);
+	}
+
+function myconfirm() {
+				var content;
+				if (confirm("有新版本更新拉！版本号:"+verr+"请复制下载地址到游览器去下载吧! \n下载地址："+down)) { //if语句内部判断确认框
+					//1							//若点击了确认，返回true，执行1
+					window.open('http://www.baidu.com','_blank');
+					
+				} 
+			}
+			
