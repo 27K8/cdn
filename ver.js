f7a0fa8f700f8aaa6ecb79116957d5699495b0bf
sysh8888 <sysh8888@126.com>
2023-04-18 16:04:11 +0800

    Update ver.js

--- ver.js
+++ ver.js
@@ -1,4 +1,4 @@
-var verr="20230418";var down="http://mao.7080.wang";
+var verr="20230418";var down="https://wwtm.lanzoum.com/i8KVl0tc4irg";
 if(ver<verr){ 
 	window.alert("有新版本更新拉！版本号:"+verr+"请复制下载地址到游览器去下载吧!下载地址："+down);
 	}
