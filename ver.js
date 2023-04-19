bc4f7d0d360d2aa78ad79bcebc90845ad55de30e
sysh8888 <sysh8888@126.com>
2023-04-20 06:39:23 +0800

    Update ver.js

--- ver.js
+++ ver.js
@@ -1,4 +1,4 @@
-var verr="20230418";var down="https://wwtm.lanzoum.com/i8KVl0tc4irg";
+var verr="20230420";var down="https://wwtm.lanzoum.com/iKmTE0th17fa";
 if(ver<verr){ 
 	window.alert("有新版本更新拉！版本号:"+verr+"请复制下载地址到游览器去下载吧!下载地址："+down);
 	}
@@ -7,7 +7,7 @@ function myconfirm() {
 				var content;
 				if (confirm("有新版本更新拉！版本号:"+verr+"请复制下载地址到游览器去下载吧! \n下载地址："+down)) { //if语句内部判断确认框
 					//1							//若点击了确认，返回true，执行1
-					window.open('http://www.baidu.com','_blank');
+					window.open('https://wwtm.lanzoum.com/iKmTE0th17fa','_blank');
 					
 				} 
 			}
