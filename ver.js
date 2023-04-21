78cbe203d7c6640e14aa469cbbea6a217fc7bbd9
sysh8888 <sysh8888@126.com>
2023-04-22 07:10:25 +0800

    Update ver.js

--- ver.js
+++ ver.js
@@ -1,14 +1,5 @@
-var verr="20230420";var down="https://wwtm.lanzoum.com/iKmTE0th17fa";
+var verr="20230422";var down="https://wwtm.lanzoum.com/b0f4xg0ed密码:6v7d";
 if(ver<verr){ 
 	window.alert("有新版本更新拉！版本号:"+verr+"请复制下载地址到游览器去下载吧!下载地址："+down);
 	}
-
-function myconfirm() {
-				var content;
-				if (confirm("有新版本更新拉！版本号:"+verr+"请复制下载地址到游览器去下载吧! \n下载地址："+down)) { //if语句内部判断确认框
-					//1							//若点击了确认，返回true，执行1
-					window.open('https://wwtm.lanzoum.com/iKmTE0th17fa','_blank');
-					
-				} 
-			}
 			
